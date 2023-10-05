55f832ac35026c7788dd25dc442fe1933a92afea
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-10-05 16:30:23 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+
+var customerName;
+
+
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+      customerName = customerName.toUpperCase();
+    }
+  }
+
+
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  setBestCustomer();
+
+
+  function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+  }  
+
+  const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer(newCustomer) {
+  try {
+    leastFavoriteCustomer = newCustomer; // Attempt to change the constant
+  } catch (error) {
+    console.error(error.message); // Log any error that occurs
+  }
+}
